004ffe4c542c737270ca12c04eea287ee4fe89b1
Rokas <dambrauskas.rokas1@gmail.com>
2021-07-05 22:23:34 +0300

    Added lecture code

---
+++ Lecture 3/Lecture 3/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Lecture_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var hello = "Hello World!";
+            // PrintText(hello);
+
+            // Console.WriteLine(Add(20, 7));
+            // var a = Convert.ToDouble(Console.ReadLine());
+            // var b = Convert.ToDouble(Console.ReadLine());
+            //
+            // var perimeter = CalculatePerimeter(a, b);
+            //
+            // Console.WriteLine(perimeter);
+
+            // var name = "Rokas";
+            // var cars = new[] {"BMW", "MAZDA", "TOYOTA" };
+            //
+            // foreach (var symbol in cars)
+            // {
+            //     Console.WriteLine(symbol);
+            // }
+        }
+        
+        // private static void HelloWorld1()
+        // {
+        //     const string name = "Hello world!";
+        // }
+        //
+        // private static string HelloWorld()
+        // {
+        //     const string name = "Hello world!";
+        //
+        //     return name;
+        // }
+
+        // private static void PrintText(string text)
+        // {
+        //     Console.WriteLine(text);
+        // }
+
+        // private static int Add(int a, int b)
+        // {
+        //     return a + b;
+        // }
+        //
+        // private static double Add(int a, int b, int c)
+        // {
+        //     return a * b;
+        // }
+        //
+        // private static double CalculatePerimeter(double a, double b)
+        // {
+        //     var c = Math.Sqrt(a * a + b * b);
+        //
+        //     return a + b + c;
+        // }
+    }
+}
