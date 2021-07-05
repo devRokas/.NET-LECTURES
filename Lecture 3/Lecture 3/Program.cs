0000efbc6542831f2ab86497d136d01e082f536f
Rokas Dambrauskas <47928198+devRokas@users.noreply.github.com>
2021-07-05 22:27:29 +0300

    Update Program.cs

--- Lecture 3/Lecture 3/Program.cs
+++ Lecture 3/Lecture 3/Program.cs
@@ -24,6 +24,91 @@ namespace Lecture_3
             // {
             //     Console.WriteLine(symbol);
             // }
+            
+                 // var number = Console.ReadLine();
+            //
+            // var test = int.Parse(number);
+            // var isSuccess = double.TryParse(number, out var result);
+            //
+            // if (!isSuccess)
+            // {
+            //     Console.WriteLine("Klaida!");    
+            // }
+            //
+            // Console.WriteLine(result);
+
+            // while (true)
+            // {
+            //     
+            // }
+            
+            
+            // int i = 5;
+            // do 
+            // {
+            //     Console.WriteLine(i);
+            //     i++;
+            // }
+            // while (i < 5);
+
+            
+
+            // for (int i = 0; i < 10; i++) 
+            // {
+            //     if (i == 4) 
+            //     {
+            //         break;
+            //     }
+            //     
+            //     Console.WriteLine(i);
+            // }
+            
+            // for (var i = 0; i < 10; i++) 
+            // {
+            //     if (i == 4) 
+            //     {
+            //         continue;
+            //     }
+            //     
+            //     Console.WriteLine(i);
+            // }
+            
+            // var name = "Rokas";
+            // var upper = name.ToUpper();
+            //
+            // Console.WriteLine(name);
+            // Console.WriteLine(upper);
+            
+            // Console.WriteLine(name.ToLower());
+            // name[1] = 't';
+            //
+            // Console.WriteLine(name.ToUpper());
+            // Console.WriteLine(name.ToLower());
+            //
+            // Console.WriteLine(name.Insert(4, " Dambrauskas"));
+            // Console.WriteLine(name.Replace('o', 't'));
+            //
+            // Console.WriteLine(name.Substring(1));
+            // Console.WriteLine(name.Substring(1, 3));
+            //
+            // Console.WriteLine(name.IndexOf('k'));
+            // Console.WriteLine(name.LastIndexOf('k'));
+            //
+            // Console.WriteLine(string.IsNullOrEmpty("       "));
+            // Console.WriteLine(string.IsNullOrWhiteSpace(""));
+            //
+            // string te;
+            // var skc = 12.1f;
+            //
+            // var myStringBuilder = new StringBuilder("Hello wordl!");
+            //
+            // myStringBuilder.Append("Hello");
+            // myStringBuilder.Append(" World!");
+            // myStringBuilder.Insert(5, "Labas");
+            //
+            // var finalString = myStringBuilder;
+            //
+            // Console.WriteLine(finalString);
         }
         
         // private static void HelloWorld1()
