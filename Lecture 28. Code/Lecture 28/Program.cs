df69b89fb41a94dc99c0d985be2f4e972381f7e9
Rokas <dambrauskas.rokas1@gmail.com>
2021-08-14 16:22:16 +0300

    Added lecture code

--- Lecture 28. Code/Lecture 28/Program.cs
+++ Lecture 28. Code/Lecture 28/Program.cs
@@ -24,35 +24,74 @@ namespace Lecture_28
             
             connection.Open();
 
-            var query = "SELECT * FROM Customers";
+            var query = @$"INSERT INTO Customers2 (first_name,last_name,email,street,city,state,age) 
+                            VALUES (@first_name, @last_name, @email, @street, @city, @state, @age)";
 
             using var command = new MySqlCommand(query, connection);
-            
-            var reader = command.ExecuteReader();
 
-            var customers = new List<Customer>();
-            
-            while (reader.Read())
-            {
-                customers.Add(new Customer
-                {
-                    CustomerId = reader.GetInt32(0),
-                    FirstName = reader.GetString(1),
-                    LastName = reader.GetString(2),
-                    Email = reader.GetString(3),
-                    Street = reader.GetString(4),
-                    City = reader.GetString(5),
-                    State = reader.GetString(6),
-                    Age = reader.GetInt32(7)
-                });
-            }
-            
+            var customers = GenerateCustomers(100);
+
             foreach (var customer in customers)
             {
-                Console.WriteLine(customer);
+                command.Parameters.AddWithValue("@first_name", customer.FirstName);
+                command.Parameters.AddWithValue("@last_name", customer.LastName);
+                command.Parameters.AddWithValue("@email", customer.Email);
+                command.Parameters.AddWithValue("@street", customer.Street);
+                command.Parameters.AddWithValue("@city", customer.City);
+                command.Parameters.AddWithValue("@state", customer.State);
+                command.Parameters.AddWithValue("@age", customer.Age);
+                
+                command.ExecuteNonQuery();
+                
+                command.Parameters.Clear();
             }
+
+            // var query = "SELECT * FROM Customers";
+            //
+            // using var command = new MySqlCommand(query, connection);
+            //
+            // var reader = command.ExecuteReader();
+            //
+            // var customers = new List<Customer>();
+            //
+            // while (reader.Read())
+            // {
+            //     customers.Add(new Customer
+            //     {
+            //         CustomerId = reader.GetInt32(0),
+            //         FirstName = reader.GetString(1),
+            //         LastName = reader.GetString(2),
+            //         Email = reader.GetString(3),
+            //         Street = reader.GetString(4),
+            //         City = reader.GetString(5),
+            //         State = reader.GetString(6),
+            //         Age = reader.GetInt32(7)
+            //     });
+            // }
+            //
+            // foreach (var customer in customers)
+            // {
+            //     Console.WriteLine(customer);
+            // }
             
             connection.Close();
+        }
+        
+        public static IEnumerable<Customer> GenerateCustomers(int count)
+        {
+            for (var i = 0; i < count; i++)
+            {
+                yield return new Customer
+                {
+                    FirstName = Faker.Name.First(),
+                    LastName = Faker.Name.Last(),
+                    Email = Faker.Internet.Email(),
+                    Street = Faker.Address.StreetAddress(),
+                    City = Faker.Address.City(),
+                    State = Faker.Country.Name(),
+                    Age = Faker.RandomNumber.Next(18, 100)
+                };
+            }
         }
     }
 }
